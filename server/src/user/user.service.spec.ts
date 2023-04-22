2728b7bd86e808d12a45ce6d962141e4228397eb
omowonuola <oyedmola@gmail.com>
2023-04-22 09:04:12 +0100

    adds unit test for signup and login user api

--- server/src/user/user.service.spec.ts
+++ server/src/user/user.service.spec.ts
@@ -13,6 +13,7 @@ import {
 import * as bcrypt from 'bcrypt';
 import { ConnectedUserService } from '../Rooms/service/connected-user/connected-user.service';
 import { MessageService } from '../Rooms/service/message/rooms.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 describe('UsersService', () => {
   let usersService: UserRepository;
@@ -50,85 +51,80 @@ describe('UsersService', () => {
     userRepositoryMock = moduleRef.get(getRepositoryToken(UserEntity));
   });
 
-  // describe('createUser', () => {
-  //   const userCredentialsDto: CreateUserDto = {
-  //     username: 'john_doe',
-  //     email: 'john_doe@example.com',
-  //     password: 'password',
-  //   };
+  describe('createUser', () => {
+    const userCredentialsDto: CreateUserDto = {
+      username: 'john_doe',
+      email: 'john_doe@example.com',
+      password: 'password',
+    };
 
-  //   it('should create a new user', async () => {
-  //     const hashedPassword = 'hashedPassword';
-  //     const createdUser = { ...userCredentialsDto, password: hashedPassword };
-  //     userRepositoryMock.findOne.mockReturnValue(undefined);
-  //     userRepositoryMock.create.mockReturnValue(createdUser);
-  //     userRepositoryMock.save.mockReturnValue(createdUser);
+    it('should create a new user', async () => {
+      const hashedPassword = 'hashedPassword';
+      const createdUser = { ...userCredentialsDto, password: hashedPassword };
+      userRepositoryMock.findOne.mockReturnValue(undefined);
+      userRepositoryMock.create.mockReturnValue(createdUser);
+      userRepositoryMock.save.mockReturnValue(createdUser);
 
-  //     const result = await usersService.createUser(userCredentialsDto);
+      const result = await usersService.createUser(userCredentialsDto);
 
-  //     expect(result).toEqual({ status: 'SUCCESS', saveUser: createdUser });
-  //   });
+      expect(result).toEqual({ status: 'SUCCESS', saveUser: createdUser });
+    });
 
-  //   it('throws a ConflictException if username or email already exists', async () => {
-  //     jest
-  //       .spyOn(userRepositoryMock, 'save')
-  //       .mockRejectedValue({ code: 'ER_DUP_ENTRY' });
+    it('throws a ConflictException if username or email already exists', async () => {
+      jest
+        .spyOn(userRepositoryMock, 'save')
+        .mockRejectedValue({ code: 'ER_DUP_ENTRY' });
 
-  //     const userCredentialsDto: CreateUserDto = {
-  //       username: 'testuser',
-  //       email: 'testuser@example.com',
-  //       password: 'password',
-  //     };
+      const userCredentialsDto: CreateUserDto = {
+        username: 'testuser',
+        email: 'testuser@example.com',
+        password: 'password',
+      };
 
-  //     await expect(usersService.createUser(userCredentialsDto)).rejects.toThrow(
-  //       ConflictException,
-  //     );
-  //   });
+      await expect(usersService.createUser(userCredentialsDto)).rejects.toThrow(
+        ConflictException,
+      );
+    });
 
-  //   it('should throw a ConflictException if any required fields are missing', async () => {
-  //     userRepositoryMock.save.mockRejectedValue({
-  //       code: 'ER_NO_DEFAULT_FOR_FIELD',
-  //     });
-  //     await expect(
-  //       usersService.createUser(userCredentialsDto),
-  //     ).rejects.toThrowError(ConflictException);
-  //   });
+    it('should throw a ConflictException if any required fields are missing', async () => {
+      userRepositoryMock.save.mockRejectedValue({
+        code: 'ER_NO_DEFAULT_FOR_FIELD',
+      });
+      await expect(
+        usersService.createUser(userCredentialsDto),
+      ).rejects.toThrowError(ConflictException);
+    });
 
-  //   it('should throw an InternalServerErrorException if any other error occurs', async () => {
-  //     userRepositoryMock.save.mockRejectedValue(new Error());
-  //     await expect(
-  //       usersService.createUser(userCredentialsDto),
-  //     ).rejects.toThrowError(InternalServerErrorException);
-  //   });
-  // });
+    it('should throw an InternalServerErrorException if any other error occurs', async () => {
+      userRepositoryMock.save.mockRejectedValue(new Error());
+      await expect(
+        usersService.createUser(userCredentialsDto),
+      ).rejects.toThrowError(InternalServerErrorException);
+    });
+  });
 
 
   describe('signInUser', () => {
     const userCredentialsDto: LoginUserDto = {
       email: 'test@test.com',
       password: 'password',
-      // username: '',
     };
-    // userRepositoryMock.findOne.mockReturnValue(undefined);
     it('should throw an UnauthorizedException if email or password are missing', async () => {
       await expect(
         usersService.signInUser({
           email: '',
           password: '',
-          // username: '',
         }),
       ).rejects.toThrowError(UnauthorizedException);
       await expect(
         usersService.signInUser({
           email: 'test@test.com',
-          // username: '',
           password: '',
         }),
       ).rejects.toThrowError(UnauthorizedException);
       await expect(
         usersService.signInUser({
           password: 'password',
-          // username: '',
           email: '',
         }),
       ).rejects.toThrowError(UnauthorizedException);
@@ -155,7 +151,6 @@ describe('UsersService', () => {
         usersService.signInUser({
           email: 'test@test.com',
           password: 'wrong_password',
-          // username: '',
         }),
       ).rejects.toThrowError(UnauthorizedException);
     });
@@ -171,7 +166,6 @@ describe('UsersService', () => {
 
       const result = await usersService.signInUser(userCredentialsDto);
 
-      // expect(result.status).toBe('SUCCESS');
       expect(result.id).toBe(user.id);
       expect(result.email).toBe(user.email);
       expect(result.accessToken).toBe('access_token');
