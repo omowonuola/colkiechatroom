ee897f934ebfecfd659a6db993a8cc15e05408f4
omowonuola <oyedmola@gmail.com>
2023-04-22 19:16:50 +0100

    adds unit test for find joined room api

--- server/src/Rooms/service/joined-room/joined-room.service.spec.ts
+++ server/src/Rooms/service/joined-room/joined-room.service.spec.ts
@@ -66,4 +66,90 @@ describe('JoinedRoomService', () => {
       expect(repository.save).toHaveBeenCalledWith(joinedRoom);
     });
   });
+
+  describe('findByUser', () => {
+    it('should find joined rooms by user', async () => {
+      const user: UserEntity = new UserEntity();
+      user.id = '123';
+      user.username = 'john_doe';
+      user.email = 'john_doe@example.com';
+      user.password = 'password';
+
+      const room: RoomEntity = {
+        id: '123',
+        name: 'My Room',
+        description: 'A description of my room',
+        users: [],
+        joinedUsers: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        messages: [],
+      };
+
+      const joinedRooms: JoinedRoomEntity[] = [
+        {
+          id: '1',
+          user,
+          room,
+          socketId: '4',
+        },
+        {
+          id: '2',
+          user,
+          room,
+          socketId: '2',
+        },
+      ];
+      jest.spyOn(repository, 'find').mockResolvedValueOnce(joinedRooms);
+
+      const result = await service.findByUser(user);
+
+      expect(result).toEqual(joinedRooms);
+      expect(repository.find).toHaveBeenCalledWith({ where: user });
+    });
+  });
+
+  describe('findByRoom', () => {
+    it('should return an array of joined rooms associated with the given room', async () => {
+      const user: UserEntity = new UserEntity();
+      user.id = '123';
+      user.username = 'john_doe';
+      user.email = 'john_doe@example.com';
+      user.password = 'password';
+
+      const room: RoomEntity = {
+        id: '123',
+        name: 'My Room',
+        description: 'A description of my room',
+        users: [],
+        joinedUsers: [],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        messages: [],
+      };
+
+      const joinedRoom1: JoinedRoomEntity = {
+        id: '1',
+        user,
+        room,
+        socketId: '1',
+      };
+      const joinedRoom2: JoinedRoomEntity = {
+        id: '2',
+        user,
+        room,
+        socketId: '2',
+      };
+      const joinedRooms: JoinedRoomEntity[] = [joinedRoom1, joinedRoom2];
+
+      jest.spyOn(repository, 'find').mockResolvedValueOnce(joinedRooms);
+
+      const result = await service.findByRoom(room);
+
+      expect(result).toEqual([joinedRoom1, joinedRoom2]);
+      expect(repository.find).toHaveBeenCalledWith({
+        where: { room },
+      });
+    });
+  });
 });
